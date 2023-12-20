c13743613efc4dee53dda5d978d68655a20c96db
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-21 02:42:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
-//your JS code here. If required.
-let inputElements = document.getElementsByClassName("code");
-console.log(inputElements.length)
-for(let i=0; i< inputElements.length; i++){
-	inputElements[i].addEventListener("keyup", (event)=>{
-		let currentElement = event.target ;
-		let code = event.key.charCodeAt(0);
-		console.log(event.key)
-		if(event.key === "Backspace"){
-			let prevElement = currentElement.previousElementSibling ;
-			if(prevElement){
-				prevElement.focus();
-			}
-			return ;
-		}
+// Your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+  let inputElements = document.querySelectorAll(".code");
 
+  for (let i = 0; i < inputElements.length; i++) {
+    inputElements[i].addEventListener("input", function (event) {
+      let currentElement = event.target;
+      let code = event.data;
 
+      if (!code) {
+        handleBackspace(currentElement);
+        return;
+      }
 
-		if(code>=48 && code<=57){
-			let nextElement = currentElement.nextElementSibling ;
-			if(nextElement){
-				nextElement.focus() ;
-			}
+      if (code >= "0" && code <= "9") {
+        focusNextInput(currentElement);
+      } else {
+        currentElement.value = "";
+      }
+    });
 
-		}else{
-			event.target.value="" ;
-		}
-	})
+    inputElements[i].addEventListener("keydown", function (event) {
+      if (event.key === "Backspace") {
+        event.preventDefault(); // Prevent the default backspace behavior
+        handleBackspace(inputElements[i]);
+      }
+    });
+  }
+});
 
+function focusNextInput(currentElement) {
+  let nextElement = currentElement.nextElementSibling;
+  if (nextElement) {
+    nextElement.focus();
+  }
+}
+
+function handleBackspace(currentElement) {
+  let prevElement = currentElement.previousElementSibling;
+  if (prevElement) {
+    prevElement.focus();
+    prevElement.value = ""; // Clear the value of the previous input
+  }
 }
