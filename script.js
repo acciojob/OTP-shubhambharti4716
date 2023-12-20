0d5ead3808c8d528f580927e8f101fff17f34dd7
Shubham Bharti <107488607+shubhambharti4716@users.noreply.github.com>
2023-12-21 01:17:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
-//your JS code here. If required.
 let inputElements = document.getElementsByClassName("code");
-console.log(inputElements.length)
-for(let i=0; i< inputElements.length; i++){
-	inputElements[i].addEventListener("keyup", (event)=>{
-		let currentElement = event.target ;
-		let code = event.key.charCodeAt(0);
-		console.log(event.key)
-		if(event.key === "Backspace"){
-			let prevElement = currentElement.previousElementSibling ;
-			if(prevElement){
-				prevElement.focus();
-			}
-			return ;
-		}
+console.log(inputElements.length);
 
+for (let i = 0; i < inputElements.length; i++) {
+  inputElements[i].addEventListener("input", (event) => {
+    let currentElement = event.target;
+    let code = event.data; // Use event.data to get the input value
 
+    if (event.inputType === "deleteContentBackward") {
+      let prevElement = currentElement.previousElementSibling;
+      if (prevElement) {
+        prevElement.focus();
+      }
+      return;
+    }
 
-		if(code>=48 && code<=57){
-			let nextElement = currentElement.nextElementSibling ;
-			if(nextElement){
-				nextElement.focus() ;
-			}
-
-		}else{
-			event.target.value="" ;
-		}
-	})
-
+    if (code >= "0" && code <= "9") {
+      let nextElement = currentElement.nextElementSibling;
+      if (nextElement) {
+        nextElement.focus();
+      }
+    } else {
+      event.target.value = "";
+    }
+  });
 }
